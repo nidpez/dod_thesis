f4292241e819ef25cff877878803d1ce237d41f8
Jhovan Mauricio Lopez Prieto <filoktulu@gmail.com>
2017-02-08 22:03:31 -0500

    Made small functions inline.

--- Math.hpp
+++ Math.hpp
@@ -13,84 +13,59 @@ struct Vec2 {
   static const Vec2 one;
 };
 
-Vec2 operator+( Vec2 a, Vec2 b );
-
-Vec2& operator+=( Vec2& a, Vec2 b );
-
-Vec2 operator-( Vec2 a, Vec2 b );
-
-Vec2& operator-=( Vec2& a, Vec2 b );
-
-Vec2& operator-( Vec2& vec );
-
-Vec2 operator*( Vec2 a, Vec2 b );
-
-Vec2& operator*=( Vec2& a, Vec2 b );
-
-Vec2 operator*( Vec2 vec, float scale );
-
-Vec2 operator*( float scale, Vec2 vec );
-
-Vec2& operator*=( Vec2& vec, float scale );
-
-float dot( Vec2 a, Vec2 b );
-
-Vec2 rotateVec2( Vec2 vec, float orientation );
-
-
 const Vec2 Vec2::zero = {};
 const Vec2 Vec2::one = { 1.0f, 1.0f };
 
-Vec2 operator+( Vec2 a, Vec2 b ) {
+inline Vec2  operator+( Vec2 a, Vec2 b ) {
   return { a.x + b.x, a.y + b.y };
 }
 
-Vec2& operator+=( Vec2& a, Vec2 b ) {
+inline Vec2& operator+=( Vec2& a, Vec2 b ) {
   a = a + b;
   return a;
 }
 
-Vec2 operator-( Vec2 a, Vec2 b ) {
+inline Vec2  operator-( Vec2 a, Vec2 b ) {
   return { a.x - b.x, a.y - b.y };
 }
 
-Vec2& operator-=( Vec2& a, Vec2 b ) {
+inline Vec2& operator-=( Vec2& a, Vec2 b ) {
   a = a - b;
   return a;
 }
 
-Vec2& operator-( Vec2& vec ) {
+inline Vec2& operator-( Vec2& vec ) {
   vec = Vec2::zero - vec;
   return vec;
 }
 
-Vec2 operator*( Vec2 a, Vec2 b ){
+inline Vec2  operator*( Vec2 a, Vec2 b ) {
   return { a.x * b.x, a.y * b.y };
 }
 
-Vec2& operator*=( Vec2& a, Vec2 b ) {
+inline Vec2& operator*=( Vec2& a, Vec2 b ) {
   a = a * b;
   return a;
 }
 
-Vec2 operator*( Vec2 vec, float scale ) {
+inline Vec2  operator*( Vec2 vec, float scale ) {
   return { vec.x * scale, vec.y * scale };
 }
 
-Vec2 operator*( float scale, Vec2 vec ) {
+inline Vec2  operator*( float scale, Vec2 vec ) {
   return { vec.x * scale, vec.y * scale };
 }
 
-Vec2& operator*=( Vec2& vec, float scale ) {
+inline Vec2& operator*=( Vec2& vec, float scale ) {
   vec = vec * scale;
   return vec;
 }
 
-float dot( Vec2 a, Vec2 b ) {
+inline float dot( Vec2 a, Vec2 b ) {
   return a.x * b.x + a.y * b.y;
 }
 
-Vec2 rotateVec2( Vec2 vec, float orientation ){
+inline Vec2 rotateVec2( Vec2 vec, float orientation ) {
   float _cos = cos( orientation );
   float _sin = sin( orientation );
   return { vec.x * _cos - vec.y * _sin, vec.y * _cos + vec.x * _sin };
