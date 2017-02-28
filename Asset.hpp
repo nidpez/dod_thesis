1cd6da8865238c0ab4af4e9932aa343b7e5d8bfa
Jhovan Mauricio Lopez Prieto <filoktulu@gmail.com>
2017-02-27 22:22:05 -0500

    Added a little documentation to loadShader.

--- Asset.hpp
+++ Asset.hpp
@@ -16,6 +16,12 @@ public:
   static void initialize();
   static void shutdown();
   static AssetIndex loadTexture( const char* name );
+  // using #ifdef technique described in https://software.intel.com/en-us/blogs/2012/03/26/using-ifdef-in-opengl-es-20-shaders
+  // so on a single shaderName.glsl file every stage goes between #ifdef and #endif, like:
+  // #ifdef STAGE_NAME
+  // 		stage code ...
+  // #endif
+  // STAGE_NAME can be one of VERTEX, GEOMETRY, or FRAGMENT
   static u32 loadShader( const char* name );
   static void destroyTexture( AssetIndex texture );
   static bool isTextureAlive( AssetIndex texture );
