aeee10c3adc4d01a1d4ad30ae5528c22ccd310f1
Jhovan Mauricio Lopez Prieto <filoktulu@gmail.com>
2017-06-14 08:04:49 -0500

    renamed EngineCommon.cpp

---
+++ EntityManager.cpp
@@ -0,0 +1,45 @@
+#include "EngineCommon.hpp"
+
+std::vector< EntityManager::Generation > EntityManager::generations;
+std::deque< u32 > EntityManager::freeIndices;
+std::unordered_multimap< u32, removeComponentCallback > EntityManager::removeComponentCallbacks;
+
+EntityHandle::operator u32() const {
+  return this->generation << HANDLE_INDEX_BITS | this->index;
+}
+
+void EntityManager::initialize() {
+}
+
+void EntityManager::shutdown() {
+}
+
+EntityHandle EntityManager::create() {
+  u32 index;
+  if ( freeIndices.size() < MIN_FREE_INDICES ) {
+    generations.push_back( { 0 } );
+    index = generations.size();    
+    ASSERT( index < MAX_ENTITIES, "Tried to create more than %d entities", MAX_ENTITIES ); 
+  } else {
+    index = freeIndices.front();
+    freeIndices.pop_front();
+  }
+  return { index, generations[ index - 1 ].generation };
+}
+
+bool EntityManager::isAlive( EntityHandle entity ) {
+  return entity.index > 0 && generations[ entity.index - 1 ].generation == entity.generation;
+}
+
+void EntityManager::destroy( EntityHandle entity ) {
+  VALIDATE_ENTITY( entity );
+  // remove all of this entity's components
+  auto range = removeComponentCallbacks.equal_range( entity );
+  for ( auto iter = range.first; iter != range.second; ++iter ) {
+    ( iter->second )( entity );
+  }
+  // remove the entity
+  u32 index = entity.index - 1; // we count from 1
+  ++generations[ index ].generation;
+  freeIndices.push_back( index );
+}
